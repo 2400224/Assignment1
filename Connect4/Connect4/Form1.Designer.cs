58f59a47f6e0cc9bbc440f223a261ce9d478bbcc
EuanRobertson1 <92741298+EuanRobertson1@users.noreply.github.com>
2022-02-02 17:57:59 +0000

    added grid

--- Connect4/Connect4/Form1.Designer.cs
+++ Connect4/Connect4/Form1.Designer.cs
@@ -35,9 +35,10 @@ namespace Connect4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1026, 647);
+            this.ClientSize = new System.Drawing.Size(658, 665);
             this.Name = "Form1";
             this.Text = "Connect4";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
--- Connect4/Connect4/Form1.cs
+++ Connect4/Connect4/Form1.cs
@@ -12,9 +12,30 @@ namespace Connect4
 {
     public partial class Form1 : Form
     {
+        //create the 2D array of buttons
+        Button[,] btn = new Button[7, 6];
+
         public Form1()
         {
             InitializeComponent();
+            for (int x = 0; x < btn.GetLength(0); x++)//x loop
+            {
+                for (int y = 0; y < btn.GetLength(1); y++)//y loop
+                {
+                    btn[x, y] = new Button(); //create the button
+                    btn[x,y].SetBounds(55+(55 * x), 55+(55* y), 45, 45); //set size and position of the buttons
+                    btn[x, y].BackColor = Color.White; //set colour of buttons to white
+                    Controls.Add(btn[x,y]); //add buttons to GUI
+             
+
+
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
